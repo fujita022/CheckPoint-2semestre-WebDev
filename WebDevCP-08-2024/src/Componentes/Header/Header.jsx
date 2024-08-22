557477a09a07e9118bb0dc81e77353b4cba9db40
Erick Fujita <rm556096@fiap.com.br>
2024-08-22 20:24:45 -0300

    f

---
+++ WebDevCP-08-2024/src/Componentes/Header/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return ( 
+        <div className="container">
+            <div className="ConteudoHeader">
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </div>
+        </div>
+     );
+}
+
+export default Header;
