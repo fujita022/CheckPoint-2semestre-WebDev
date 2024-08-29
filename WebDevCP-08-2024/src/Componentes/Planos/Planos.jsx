a5a7b4de5f23f2aaacda78c361e5ba5663fb7dab
Erick Fujita <rm556096@fiap.com.br>
2024-08-29 19:28:23 -0300

    f

--- WebDevCP-08-2024/src/Componentes/Planos/Planos.jsx
+++ WebDevCP-08-2024/src/Componentes/Planos/Planos.jsx
@@ -10,18 +10,26 @@ function Planos({headerplanos, bodyplanosp,bodyplanosh6,bodyplanosh6preco, bodyp
           <h1>{headerplanos}</h1>
         </div>
         <div className="body-planos">
-          <p>{bodyplanosp}</p>
-          <img className='img-card-ultimo' src={ImagemCard} alt="" />
-          <div className="seta-planos">
-          <h6 className='preco-planos'>{bodyplanosh6} <h2>{bodyplanosh6preco}</h2></h6>
-          <img src={ImagemSeta} alt="Imagem seta layout" />
+          {ImagemCard ? (
+            <div className="imagem-card-body">
+              <img src={ImagemCard} alt="Imagem card" />
+            </div>
+          ): (
+              <>
+                <p>{bodyplanosp}</p>
+                <img className='img-card-ultimo' src={ImagemCard} alt="" />
+                <div className="seta-planos">
+                <h6 className='preco-planos'>{bodyplanosh6} <h2>{bodyplanosh6preco}</h2></h6>
+                <img src={ImagemSeta} alt="Imagem seta layout" />
+                </div>
+                <p>{bodyplanosp2}</p>
+                <div className="seta-planos">
+                <h6 className='preco-planos'>{bodyplanosh62} <h2>{bodyplanosh6preco2}</h2></h6>
+                <img src={ImagemSeta} alt="Imagem seta layout" />
+                </div>
+              </>
+          )}
           </div>
-          <p>{bodyplanosp2}</p>
-          <div className="seta-planos">
-          <h6 className='preco-planos'>{bodyplanosh62} <h2>{bodyplanosh6preco2}</h2></h6>
-          <img src={ImagemSeta} alt="Imagem seta layout" />
-          </div>
-        </div>
         <div className="button-cadastrar-planos" style={{backgroundColor: buttonColor, color: corTexto, border: borderColor}}>
           {buttonplanos}
         </div>
